1f6109525f1cab3461c49a4dd233cc870a49759a
minzzle <integration@minzzle.com>
2024-01-07 15:42:54 +0530

    get api

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, Naveen!"}
+
+@app.get("/about")
+def read_about():
+    return {"message": "About, Naveen!"}
+
+@app.get("/services")
+def read_services():
+    return {"message": "Services!"}
